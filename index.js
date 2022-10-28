c43a40d3278dae818cc7ac8710362e7d585b9d89
FarhanZizz <iamfarhanaziz@gmail.com>
2022-10-28 21:59:17 +0600

    added vercel

--- index.js
+++ index.js
@@ -4,16 +4,24 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 app.use(cors())
-const categories = require('./data/categories.json')
+const categories = require('./data/categories.json');
+const courses = require('./data/courses.json')
 app.get('/', (req, res) => {
-    res.send('api running')
+    res.send(courses)
 });
 app.get('/categories/', (req, res) => {
     res.send(categories)
 })
+
 app.get('/categories/:id', (req, res) => {
     const category_id = req.params.id;
-    console.log(category_id)
+    const selected_courses = courses.filter(c => c.category_id === category_id);
+    res.send(selected_courses)
+})
+app.get('/courses/:id', (req, res) => {
+    const course_id = req.params.id;
+    const courseDetails = courses.find(course => course.id === course_id);
+    res.send(courseDetails)
 })
 app.listen(port, () => {
     console.log('running on port ', port)
