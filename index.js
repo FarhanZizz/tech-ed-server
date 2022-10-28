4749fcc9838b0f4ba351f97565d3b1873804267f
FarhanZizz <iamfarhanaziz@gmail.com>
2022-10-28 16:08:13 +0600

    added couses data

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+app.use(cors())
+const categories = require('./data/categories.json')
 app.get('/', (req, res) => {
     res.send('api running')
 });
-
+app.get('/categories/', (req, res) => {
+    res.send(categories)
+})
+app.get('/categories/:id', (req, res) => {
+    const category_id = req.params.id;
+    console.log(category_id)
+})
 app.listen(port, () => {
     console.log('running on port ', port)
 })
